0215497a483951291f4b35531eed9cc3f1c264a9
RamkrishnaRocket1 <rdakwa@rocketsoftware.com>
2026-03-02 12:49:24 +0530

    Republish_test_update
    Signed-off-by: RamkrishnaRocket1 <rdakwa@rocketsoftware.com>

--- release.config.js
+++ release.config.js
@@ -4,21 +4,10 @@ module.exports = {
             name: "master",
             level: "minor"
         }
-        // {
-        //     name: "zowe-v?-lts",
-        //     level: "patch"
-        // }
-        // {
-        //     name: "next",
-        //     prerelease: true
-        // }
     ],
     plugins: [
         "@octorelease/changelog",
         ["@octorelease/npm", {
-            aliasTags: {
-                "latest": ["next"]
-            },
             smokeTest: true
         }],
         ["@octorelease/github", {
